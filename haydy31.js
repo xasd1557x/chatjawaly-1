1bb40e416a66535bc56b306c0a725e6545729f28
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-09 01:15:38 +0300

    Update and rename haydy30.js to haydy31.js

--- haydy31.js
+++ haydy31.js
@@ -43,6 +43,10 @@ z-index: 0 !important;
     margin-top: 2px!important;
     margin-bottom: 2px!important;
     }
+#users .uzr.haydy1 .c-flex.fr {
+  height: 0px !important;
+  margin-left: -4px!important;
+}    
 #users .uzr.haydy1 .light.fl.pro.break {
 background-image: url(https://up6.cc/2023/12/170207118749494.png);
 }
