522c94ba61b9e38502b5a65484317bd8836ec254
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-09 00:56:23 +0300

    Update and rename haydy19.js to haydy20.js

--- haydy20.js
+++ haydy20.js
@@ -59,9 +59,9 @@ background-image: url(https://up6.cc/2023/12/170207118749494.png);
 }
 #users .uzr.haydy1 .haydy_i6ar1 {
 background-size: 100%;
-width: 65px;
+width: 70px;
 margin-top: -10px;
-height: 69px;
+height: 74px;
 margin-left: -10px;
 }
 
