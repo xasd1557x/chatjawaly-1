1ab88ce4bccb48b40ebceb554d16a4b1d417629a
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-08 23:28:54 +0300

    Update and rename haydy1.js to haydy2.js

---
+++ haydy2.js
@@ -0,0 +1,77 @@
+
+$("style").last().append(`
+#users .uzr.haydy1 .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 2px !important;
+  height: 55px !important;
+  margin-left: -5px !important;
+  width: 55px;
+}
+#users .uzr.haydy1 .d-flex.dots {
+  margin-top: 7px !important;
+  margin-left: 5px;
+}
+#users .uzr.haydy1 .u-msg {
+    text-align: center;
+    background-image: url(https://up6.cc/2023/12/170162505647371.gif);margin-right: 3px!important;
+    margin-left: 2px!important;
+    margin-top: 10px!important;
+    margin-bottom: 10px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 170% auto;
+    }
+#users .uzr.haydy1 .ustat {
+width: 12px !important;
+border-radius: 5px 0px 5px 0px;
+height: 12px !important;
+min-height: 0% !important;
+z-index: 0 !important;
+}
+#users .uzr.haydy1 span.uhash {
+  margin-right: 324px;
+  margin-top: 24px;
+  margin-left: 0px;
+}
+#users .uzr.haydy1 img.co {
+  transform: rotate(90deg);
+  margin-top: -60px !important;
+  margin-right: 282px;
+}
+#users .uzr.haydy1 {
+    background-size: 100%;
+    border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
+    background-image: url(https://up6.cc/2023/12/170171058180011.jpg);
+    margin-right: 0px!important;
+    margin-left: 0px!important;
+    margin-top: 2px!important;
+    margin-bottom: 2px!important;
+    }
+#users .uzr.haydy1 .c-flex.fr {
+  margin-left: -4px!important;
+  float: left;
+}
+#users .uzr.haydy1 .light.fl.pro.break {
+background-image: url(https://up6.cc/2023/12/170171058180011.jpg);
+}
+#users .uzr.haydy1 .d-flex.fl {
+  padding-right: 0px !important;
+}
+#users .uzr.haydy1 .u-topic {
+  margin-left: 5px;
+}
+#users .uzr.haydy1 .haydy_i6ar1 {
+width: 61px;
+margin-top: -2px;
+height: 60px;
+margin-left: -4px;
+}
+
+
+`);
+var l_itvhaydy=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvhaydy);
+        $("#users .uzr:contains('ــــــ')").addClass('haydy1');
+        $("#users .uzr.haydy1 .fitimg.u-pic").append('<img class="haydy_i6ar1" src="https://up6.cc/2023/02/167579621323341.png">');
+    } ); 
+});
+    }
+}, 10000);
