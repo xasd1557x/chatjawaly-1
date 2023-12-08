3fcd8e668a5778deb62e909f0c3130fb9b248216
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-09 00:10:55 +0300

    Update and rename haydy11.js to haydy12.js

--- haydy12.js
+++ haydy12.js
@@ -71,5 +71,9 @@ var l_itvhaydy=setInterval(function(){
         clearInterval(l_itvhaydy);
         $("#users .uzr:contains('ــــــ')").addClass('haydy1');
         $("#users .uzr.haydy1 .fitimg.u-pic").append('<img class="haydy_i6ar1" src="https://up6.cc/2023/02/167579621323341.png">');
+        $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
-10000);
+    } ); 
+});
+    }
+}, 10000);
