0db8056a9050665ccde3896769b3809e232494fc
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-13 21:33:03 +0300

    Update and rename tjarb11.js to tjarb12.js

---
+++ tjarb12.js
@@ -0,0 +1,82 @@
+
+$("style").last().append(`
+#users .haydy2 .fitimg.u-pic {width: 58px !important;
+    height: 58px !important;border-radius: 15px 0px 15px 0px !important;
+    border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;background-size: 100%;margin-top: 5px!important;margin-bottom: 4px!important;
+}
+#users .haydy2 .d-flex.dots {
+  margin-top: 7px !important;
+  margin-left: 5px;
+}
+#users .haydy2 .u-msg {
+    text-align: center;
+    background-image: url(https://up6.cc/2023/12/170207118685932.gif);margin-right: 3px!important;
+    margin-left: 2px!important;
+    margin-top: 10px!important;
+    margin-bottom: 10px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 170% auto;
+    }
+#users .haydy2 .ustat {
+width: 12px !important;
+border-radius: 5px 0px 5px 0px;
+height: 12px !important;
+min-height: 0% !important;
+margin-left: 3px!important;
+margin-top: 5px!important;
+border: 1px solid #fff;
+box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
+}
+#users .haydy2 span.uhash {
+  margin-right: 324px;
+  margin-top: 24px;
+  margin-left: 0px;
+}
+#users .uzr.haydy2 img.co {
+  margin-top: 0px !important;
+  margin-right: 100px;
+}
+#users .haydy2 {
+    background-size: 100% auto;
+    border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
+    background-image: url(https://up6.cc/2023/12/170215491418873.gif);
+    margin-right: 0px!important;
+    margin-left: 0px!important;
+    margin-top: 2px!important;
+    margin-bottom: 2px!important;
+    }
+#users .haydy2.d-flex.mm {
+    border-radius: 5px 5px 5px 5px;
+    margin-top: 2px!important;
+    margin-bottom: 2px!important;
+    border-bottom: 0.5px solid #787878!important;
+    border: 0.5px solid #787878;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #acacac;
+    }    
+#users .haydy2 .c-flex.fr {
+  height: 0px !important;
+  margin-left: -4px!important;
+}    
+#upro.haydy2 .light.fl.pro.break {
+    background-image: url(https://up6.cc/2023/12/170215305562121.jpg);
+    background-size: 100%;
+}
+#users .haydy2 .d-flex.fl {
+  padding-right: 0px !important;
+}
+#users .haydy2 .u-topic {
+	-webkit-text-fill-color: transparent;
+	-webkit-background-clip: text;
+	color: transparent !important;
+  margin-left: 5px;
+}
+
+
+`);
+var l_itvhaydy=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvhaydy);
+        $("#users, #upro :contains('هاوي')").addClass('haydy2');
+        $("#ifr_mov1").show(function(){
+        $("#upro").modal("hide");
+    } ); 
+    }
+}, 10000);
