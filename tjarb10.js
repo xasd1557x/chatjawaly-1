106d24e3fd506875d0aecb0503933080af81bb12
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-13 21:27:21 +0300

    Update and rename tjarb9.js to tjarb10.js

---
+++ tjarb10.js
@@ -0,0 +1,13 @@
+$("style").last().append(`    
+#upro.uzr.haydy2 .light.fl.pro.break {
+    background-image: linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);
+    background-image: url(https://up6.cc/2023/11/170009087924394.png);
+    background-size: 100%;
+}
+`);
+var l_itvhaydy = false;
+setInterval(function () {
+    if(myid!=null){
+        clearInterval(l_itvhaydy);
+        $("#upro.uzr:contains('هااوي')").addClass('haydy2');
+    }, 10);
