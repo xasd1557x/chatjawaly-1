781bd69047c42c6072a429e09c7be0f2df4510d6
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-13 20:56:54 +0300

    Update and rename tjarb2.js to tjarb3.js

--- tjarb3.js
+++ tjarb3.js
@@ -1,5 +1,5 @@
 $("style").last().append(`    
-#upro .uzr.haydy2 .light.fl.pro.break {
+#upro.uzr.haydy2 .light.fl.pro.break {
     background-image: linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);
     background-image: url(https://up6.cc/2023/11/170009087924394.png);
     background-size: 100%;
@@ -8,7 +8,7 @@ $("style").last().append(`
 var l_itvhaydy=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvhaydy);
-        $("#upro .uzr:contains('هااوي')").addClass('haydy2');
+        $("#upro.uzr:contains('هااوي')").addClass('haydy2');
         $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
     } ); 
