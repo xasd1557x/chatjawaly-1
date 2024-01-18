a8eeff0225f5a6072b2890f3cbe1ced29ddd6ae4
xasd123x <153305949+xasd123x@users.noreply.github.com>
2024-01-18 23:17:23 +0300

    Update enas.js

--- enas.js
+++ enas.js
@@ -1,21 +1,21 @@
 
 $("style").last().append(`
-#users .uzr.haydy2 .fitimg.u-pic {width: 58px !important;
+#users .uzr.enas2 .fitimg.u-pic {width: 58px !important;
     height: 58px !important;border-radius: 15px 0px 15px 0px !important;
     border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;background-size: 100%;margin-top: 5px!important;margin-bottom: 4px!important;
 }
-#users .uzr.haydy2 .d-flex.dots {
+#users .uzr.enas2 .d-flex.dots {
   margin-top: 7px !important;
   margin-left: 5px;
 }
-#users .uzr.haydy2 .u-msg {
+#users .uzr.enas2 .u-msg {
     text-align: center;
     background-image: url(https://up6.cc/2023/12/170207118685932.gif);margin-right: 3px!important;
     margin-left: 2px!important;
     margin-top: 10px!important;
     margin-bottom: 10px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 170% auto;
     }
-#users .uzr.haydy2 .ustat {
+#users .uzr.enas2 .ustat {
 width: 12px !important;
 border-radius: 5px 0px 5px 0px;
 height: 12px !important;
@@ -25,16 +25,16 @@ margin-top: 5px!important;
 border: 1px solid #fff;
 box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
 }
-#users .uzr.haydy2 span.uhash {
+#users .uzr.enas2 span.uhash {
   margin-right: 324px;
   margin-top: 24px;
   margin-left: 0px;
 }
-#users .uzr.haydy2 img.co {
+#users .uzr.enas2 img.co {
   margin-top: 0px !important;
   margin-right: 100px;
 }
-#users .uzr.haydy2 {
+#users .uzr.enas2 {
     background-size: 100% auto;
     border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
     background-image: url(https://up6.cc/2024/01/170560248842221.png);
@@ -43,7 +43,7 @@ box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
     margin-top: 2px!important;
     margin-bottom: 2px!important;
     }
-#users .uzr.haydy2.d-flex.mm {
+#users .uzr.enas2.d-flex.mm {
     border-radius: 5px 5px 5px 5px;
     margin-top: 2px!important;
     margin-bottom: 2px!important;
@@ -51,18 +51,18 @@ box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
     border: 0.5px solid #787878;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #acacac;
     }    
-#users .uzr.haydy2 .c-flex.fr {
+#users .uzr.enas2 .c-flex.fr {
   height: 0px !important;
   margin-left: -4px!important;
 }    
-#upro.haydy2 .light.fl.pro.break {
+#upro.enas2 .light.fl.pro.break {
     background-image: url(https://up6.cc/2023/12/170215305562121.jpg);
     background-size: 100%;
 }
-#users .uzr.haydy2 .d-flex.fl {
+#users .uzr.enas2 .d-flex.fl {
   padding-right: 0px !important;
 }
-#users .uzr.haydy2 .u-topic {
+#users .uzr.enas2 .u-topic {
 	-webkit-text-fill-color: transparent;
 	-webkit-background-clip: text;
 	color: transparent !important;
@@ -71,10 +71,10 @@ box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
 
 
 `);
-var l_itvhaydy=setInterval(function(){
+var l_itvenas=setInterval(function(){
     if(myid!=null){
-        clearInterval(l_itvhaydy);
-        $("#users .uzr:contains('⠀♡ є и α s | إيـــنۨــاسۜ ♡⠀▪︎ّ⠀ .')").addClass('haydy2');
+        clearInterval(l_itvenas);
+        $("#users .uzr:contains('⠀♡ є и α s | إيـــنۨــاسۜ ♡⠀▪︎ّ⠀. .')").addClass('enas2');
         $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
     } ); 
